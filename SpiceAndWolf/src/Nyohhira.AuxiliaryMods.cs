aed45a06c6f91e80bdf45f4367c0d32ee2c74333
psynull <564310600@qq.com>
2020-04-19 01:52:35 +0800

    Add template

---
+++ SpiceAndWolf/src/Nyohhira.AuxiliaryMods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace Nyohhira.AuxiliaryMods
+{
+    public class RandomEvent : MBSubModuleBase // 奇怪的画提出的随机事件
+    {
+
+    }
+
+    public class SlaveTrader : MBSubModuleBase // 这个是我自己的脑洞，如果有人支持就把坑填上
+    {
+
+    }
+}
---
+++ SpiceAndWolf/src/Nyohhra.SpiceAndWolf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace Nyohhra.SpiceAndWolf
+{
+    public class SpiceAndWolf : MBSubModuleBase // 本 mod 本体类
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Gallery", new TextObject("Gallery", null), 9990, () =>
+            {
+                InformationManager.DisplayMessage(new InformationMessage("No CG yet"));
+            }, false));
+        }
+    }
+}
